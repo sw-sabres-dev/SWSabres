209e1114b8840c3b53a23ef9da863c29fbbf8510
Mark Johnson <mark@dev.proscape.com>
2015-11-20 11:09:16 -0500

    Added 1 day check for whether you can edit a score.
    Re-enabled keyboard on edit score to be number pad

--- SWSabres/ViewControllers/GameDetailViewController.swift
+++ SWSabres/ViewControllers/GameDetailViewController.swift
@@ -76,163 +76,6 @@ class GameDetailViewController: UIViewController
         
         self.populateControlsWithGame()
 
-        /*
-        if let delegate:AppDelegate = UIApplication.sharedApplication().delegate as? AppDelegate, contentManager: ContentManager = delegate.contentManager, let game = game
-        {
-            
-            if let schedule: Schedule = contentManager.scheduleMap[game.gameScheduleId], let team: Team = contentManager.teamMap[schedule.scheduleTeamId], let shortName = team.shortName
-            {
-                if game.isHomeGame
-                {
-                    firstLogo.image = UIImage(named: "logo")
-                    firstLogoLabel.text = shortName
-                    if let score = game.gameOurScore
-                    {
-                        firstScoreLabel.text = String(score)
-                    }
-                    else
-                    {
-                        firstScoreLabel.hidden = true
-                    }
-                }
-                else
-                {
-                    secondLogo.image = UIImage(named: "logo")
-                    secondLogoLabel.text = shortName
-                    if let score = game.gameOurScore
-                    {
-                        secondScoreLabel.text = String(score)
-                    }
-                    else
-                    {
-                        secondScoreLabel.hidden = true
-                    }
-                }
-            }
-            
-            if let teamId: String = game.teamId, let team: Team = contentManager.teamMap[teamId], let teamName: String = team.shortName ?? team.name
-            {
-                var logoUrl: NSURL? = nil
-                
-                if let teamLogoUrlString: String = team.logoUrl
-                {
-                    logoUrl = NSURL(string: teamLogoUrlString)
-                }
-                
-                if game.isHomeGame
-                {
-                    secondLogo.image = nil
-                    secondLogo.pin_setImageFromURL(logoUrl)
-                    secondLogoLabel.text = teamName
-                    if let score = game.gameOppScore
-                    {
-                        secondScoreLabel.text = String(score)
-                    }
-                    else
-                    {
-                        secondScoreLabel.hidden = true
-                    }
-                }
-                else
-                {
-                    firstLogo.image = nil
-                    firstLogo.pin_setImageFromURL(logoUrl)
-                    firstLogoLabel.text = teamName
-                    if let score = game.gameOppScore
-                    {
-                        firstScoreLabel.text = String(score)
-                    }
-                    else
-                    {
-                        firstScoreLabel.hidden = true
-                    }
-                }
-            }
-            else if let opponent = game.opponent
-            {
-                if game.isHomeGame
-                {
-                    secondLogo.pin_setImageFromURL(nil)
-                    secondLogo.image = nil
-                    secondLogoLabel.text = opponent
-                    if let score = game.gameOppScore
-                    {
-                        secondScoreLabel.text = String(score)
-                    }
-                    else
-                    {
-                        secondScoreLabel.hidden = true
-                    }
-                }
-                else
-                {
-                    firstLogo.pin_setImageFromURL(nil)
-                    firstLogo.image = nil
-                    firstLogoLabel.text = opponent
-                    if let score = game.gameOppScore
-                    {
-                        firstScoreLabel.text = String(score)
-                    }
-                    else
-                    {
-                        firstScoreLabel.hidden = true
-                    }
-                }
-            }
-            
-            let compareResult = game.gameDate.compare(NSDate())
-            addScoreButton.enabled = compareResult == .OrderedAscending || compareResult == .OrderedSame
-            
-            if game.gameOurScore != nil && game.gameOppScore != nil
-            {
-                addScoreButton.title = "Edit Score"
-            }
-            
-//            firstScoreTextField.hidden = compareResult == .OrderedDescending
-//            secondScoreTextField.hidden = compareResult == .OrderedDescending
-
-            
-            if let firstText: String = firstLogoLabel.text, let secondText: String = secondLogoLabel.text
-            {
-                self.title = "\(firstText) vs. \(secondText)"
-            }
-            
-            let dateFormatter: NSDateFormatter = NSDateFormatter()
-            dateFormatter.dateFormat = "MMMM dd"
-            
-            self.dateLabel.text = dateFormatter.stringFromDate(game.gameDate)
-            
-
-            if let gameResult = game.gameResult
-            {
-                timeLabel.text = gameResult
-            }
-            else
-            {
-                dateFormatter.dateFormat = nil
-                dateFormatter.timeStyle = .ShortStyle
-
-                timeLabel.text = dateFormatter.stringFromDate(game.gameDate)
-            }
-            
-            if let gameVenueId = game.gameVenueId, let venue: Venue = contentManager.venueMap[gameVenueId]
-            {
-                venueTitleLabel.text = venue.title
-                let address = "\(venue.address) \(venue.city) \(venue.state) \(venue.zip)"
-                addressLabel.text = address
-                
-                showAddressOnMap(address)
-            }
-            else
-            {
-                venueTitleLabel.text = ""
-                addressLabel.text = ""
-            }
-
-            
-        }
-        */
-        
         // Do any additional setup after loading the view.
     }
 
@@ -346,8 +189,13 @@ class GameDetailViewController: UIViewController
                 }
             }
             
-            let compareResult = game.gameDate.compare(NSDate())
-            addScoreButton.enabled = compareResult == .OrderedAscending || compareResult == .OrderedSame
+            let now = NSDate()
+            let compareResult = game.gameDate.compare(now)
+            
+            if let gameDatePlusOneDay = NSCalendar.currentCalendar().dateByAddingUnit(.Day, value: 1, toDate: game.gameDate, options:[])
+            {
+                addScoreButton.enabled = compareResult != .OrderedDescending && now.compare(gameDatePlusOneDay) != .OrderedDescending
+            }
             
             if game.gameOurScore != nil && game.gameOppScore != nil
             {
@@ -528,13 +376,13 @@ class GameDetailViewController: UIViewController
         alertController.addAction(action)
         
         alertController.addTextFieldWithConfigurationHandler { (textField) -> Void in
-            //textField.keyboardType = .NumberPad
+            textField.keyboardType = .NumberPad
             textField.placeholder = self.firstLogoLabel.text
             textField.text = self.firstScoreLabel.text
         }
         
         alertController.addTextFieldWithConfigurationHandler { (textField) -> Void in
-            //textField.keyboardType = .NumberPad
+            textField.keyboardType = .NumberPad
             textField.placeholder = self.secondLogoLabel.text
             textField.text = self.secondScoreLabel.text
         }
